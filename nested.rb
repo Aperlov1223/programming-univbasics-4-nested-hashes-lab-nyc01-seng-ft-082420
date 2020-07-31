5ab58b48aef69b9278539d5b23eaba326dd02e7a
Alice Perlov <aperlov23@gmail.com>
2020-07-31 19:21:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,9 +16,10 @@ def hopper
       }
     }
 
+programmer_hash[:grace_hopper]
+
 end
 
-programmer_hash[:grace_hopper]
 
 
 def alan_kay_is_known_for
@@ -40,9 +41,9 @@ def alan_kay_is_known_for
       }
     }
 
-end
-
 programmer_hash[:alan_kay_is_known_for]
+
+end
 
 def dennis_ritchies_language
 	programmer_hash =
